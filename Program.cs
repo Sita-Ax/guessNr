1546607976b995d2344e16a06b318ecdae1c450f
sita-Ax <sita81@hotmail.com>
2022-09-06 09:33:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GuessNr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter username: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine(userName + " get a number between 1-10");
+
+            int number = Convert.ToInt16(Console.ReadLine());
+
+             if(number < 11)
+            {
+                Console.WriteLine("Valid");
+            }
+             else if(number > 10)
+            Console.WriteLine("Invalid");
+        }
+    }
+}
