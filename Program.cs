f88a3cdb93abc3cd43e9c55e231f09b473bc3eda
sita-Ax <sita81@hotmail.com>
2022-09-07 13:11:41 +0200

    Exercises 1 and 2

--- Program.cs
+++ Program.cs
@@ -2,22 +2,83 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Menu()
         {
-            
-            Console.WriteLine("Enter username: ");
-            string userName = Console.ReadLine();
+            Console.WriteLine("-----Menu------");
+            Console.WriteLine("Press on 1 to look if the number is valied or not! ");
+            Console.WriteLine("Press on 2 to guess until the number is valied!");
+            Console.WriteLine("Press on 9 to exit!");
+        }
 
-            Console.WriteLine(userName + " get a number between 1-10");
-
+        static void Valied()
+        {
             int number = Convert.ToInt16(Console.ReadLine());
-
-             if(number < 11)
+            if (number < 11)
             {
                 Console.WriteLine("Valid");
             }
-             else if(number > 10)
-            Console.WriteLine("Invalid");
+            else
+                Console.WriteLine("Invalid");
+        }
+
+        static void Guess()
+        {
+            int number = Convert.ToInt16(Console.ReadLine());
+            bool loop = true;
+            while (loop)
+            {
+                if (number <= 10)
+                {
+                    Console.WriteLine("Yes it is valied.");
+                    loop = false;
+                }
+                else
+                {
+                    Console.WriteLine("It is not a valied number, try again.");
+                    number = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            string? userName = Console.ReadLine();
+            bool loop = true;
+            do
+            {
+                try
+                {
+                    Console.Clear(); 
+                    Menu();
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine(userName + " get a number between 1-10");
+                            Valied();
+                            break;
+                        case 2:
+                            Console.WriteLine(userName + " guess a number between 1-10");
+                            Guess();
+                            break;
+                        case 9:
+                            loop = false;
+                            Console.WriteLine("Welcome back");
+                            break;
+                        default:
+                            Console.WriteLine("Somthings get wrong here!");
+                            break;
+                    }
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }while (loop);
         }
     }
 }
